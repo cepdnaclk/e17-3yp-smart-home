b32d44a29a4a70885f592d3865ee73d6c30f4076
Nishan-Charlie <e17230@eng.pdn.ac.lk>
2021-09-20 00:31:11 +0530

    A Room model Not Finished jet

---
+++ Backend/models/rooms.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+// var bcrypt = require('bcrypt');
+
+var roomsSchema = new schema({
+	name: {
+		type: String,
+		require,
+	},
+});
