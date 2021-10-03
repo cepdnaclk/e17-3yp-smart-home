5f700944559f9d5dff2ed0748964f8d99fb1284f
Nishan-Charlie <e17230@eng.pdn.ac.lk.com>
2021-10-03 12:31:03 +0530

    home schema added

--- Backend/models/homes.js
+++ Backend/models/homes.js
@@ -1,6 +1,43 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
-var roomSchema = require('./rooms');
+// var roomSchema = require('./rooms');
+
+let deviceSchema = new schema({
+	name: {
+		type: String,
+		require: true,
+	},
+	type: {
+		type: String,
+		require: true,
+	},
+	Energy: {
+		type: Number,
+	},
+	status: {
+		type: Number,
+		default: 0,
+	},
+});
+
+var roomsSchema = new schema({
+	roomname: {
+		type: String,
+		require: true,
+		unique: true,
+	},
+	roomType: {
+		type: String,
+		require: true,
+	},
+	devices: [deviceSchema],
+
+	numberOfDevices: {
+		type: Number,
+		default: 0,
+	},
+});
+
 var homeschema = new schema({
 	homename: {
 		type: String,
@@ -10,7 +47,7 @@ var homeschema = new schema({
 	address: {
 		type: String,
 	},
-	// rooms: [roomSchema],
+	rooms: [roomsSchema],
 });
 
 module.exports = mongoose.model('homes', homeschema);
