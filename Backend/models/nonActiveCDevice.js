70dc6c5b20057d72b990ee119624f2e260a0066d
Nishan-Charlie <e17230@eng.pdn.ac.lk.com>
2021-10-22 16:27:51 +0530

    update

--- Backend/models/nonActiveCDevice.js
+++ Backend/models/nonActiveCDevice.js
@@ -21,5 +21,28 @@ nonActiveDevice.methods.comparePassword = function (candidatePassword, cb) {
 	});
 };
 
+
+
+//Using bcrypt algorithm to encrypt the cdevice password
+nonActiveDevice.pre('save', function (next) {
+	var cdevice = this;
+	if (this.isModified('password') || this.isNew) {
+		bcrypt.genSalt(10, function (err, salt) {
+			if (err) {
+				return next(err);
+			}
+			bcrypt.hash(cdevice.password, salt, function (err, hash) {
+				if (err) {
+					return next(err);
+				}
+				cdevice.password = hash;
+				next();
+			});
+		});
+	} else {
+		return next();
+	}
+});
+
 //Creating Model With name of "Users"
 module.exports = mongoose.model('NonActiveCentralDevices', nonActiveDevice);
