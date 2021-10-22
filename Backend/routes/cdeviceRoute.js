7fe5824a841806d15faa61b99f551810b5e50098
Nishan-Charlie <e17230@eng.pdn.ac.lk.com>
2021-10-22 15:48:24 +0530

    update

--- Backend/routes/cdeviceRoute.js
+++ Backend/routes/cdeviceRoute.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const cdControl = require('../controllers/cdeviceControl');
+const userControl = require('../controllers/userControl');
 
 //Adding central device
-router.post('/api/cdevice/addCdevice', cdControl.addCdevice);
+router.post('/api/cdevice/addCdevice', userControl.verifyToken, cdControl.addCdevice);
 
 //getting all the rooms
 router.get;
