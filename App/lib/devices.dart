3e61b9a35b758d4228d7e36185cc3ac62869f1b9
varnaraj <e17358@eng.pdn.ac.lk>
2021-10-22 05:30:58 +0530

    test
    test1

--- App/lib/devices.dart
+++ App/lib/devices.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-//import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
-
 //Add device
 
 class Sample extends StatefulWidget {
