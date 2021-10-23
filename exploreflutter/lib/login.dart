80709dbe0bfd894413bde80f844775d859fccb95
varnaraj <e17358@eng.pdn.ac.lk>
2021-10-23 22:03:04 +0530

    update

--- exploreflutter/lib/login.dart
+++ exploreflutter/lib/login.dart
@@ -9,18 +9,8 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-//The logic and internal state for our StatefulWidget(LoginPage).
-//State is information that (1) can be read synchronously when the widget is
-//built and (2) might change during the lifetime of the widget.
-class _LoginPageState extends State<LoginPage> {
-  //`GlobalKey()` will create us a LabeledGlobalKey, which is a GlobalKey
-  //with a label used for debugging.
-  // A GlobalKey is a key that is unique across the entire app.
-  //`FormState` is a state associated with a Form widget.
+class _LoginPageState extends State<LoginPage> {t.
   final formKey = GlobalKey<FormState>();
-  //`SaffoldState` is a state for a Scaffold.
-  // Remember `Scaffold`is a class that implements the basic material design
-  //visual layout structure and derives from the `StatefulWidget`
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   //We have two private fields here
@@ -32,14 +22,11 @@ class _LoginPageState extends State<LoginPage> {
     //get state of our Form
     final form = formKey.currentState;
 
-    //`validate()` validates every FormField that is a descendant of this Form,
-    // and returns true if there are no errors.
+   
     if (form!.validate()) {
-      //`save()` Saves every FormField that is a descendant of this Form.
+
       form.save();
 
-      // Email & password matched our validation rules
-      // and are saved to _email and _password fields.
       _loginCommand();
     }
   }
--- exploreflutter/lib/main.dart
+++ exploreflutter/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:exploreflutter/login.dart';
 
+
+
+
 //Our app
 class MyApp extends StatelessWidget {
   @override
