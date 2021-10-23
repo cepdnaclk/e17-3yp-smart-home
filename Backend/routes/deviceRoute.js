ed1d78d0ec83109b45dfd1cdbb97b91c8380aaad
Nishan-Charlie <e17230@eng.pdn.ac.lk.com>
2021-10-23 20:37:51 +0530

    update

--- Backend/routes/deviceRoute.js
+++ Backend/routes/deviceRoute.js
@@ -3,11 +3,15 @@ const router = express.Router();
 // const roomControl = require('../controllers/roomControl');
 const userControl = require('../controllers/userControl')
 const deviceControl = require('../controllers/deviceControl')
+const deviceFunction = require('../controllers/deviceFunction')
 
 //Adding a new room
 router.post('/api/devices/adddevice', userControl.verifyToken, deviceControl.addDevice);
 
 //Get all devices
 router.post('/api/devices/getallDevices', userControl.verifyToken, deviceControl.getAllDevices )
+
+//Turn on A device
+// router.post('/api/devices/turnOn', userControl.verifyToken, deviceFunction.turnOn)
 
 module.exports = router
