2a92d5a86cc73176aec72cdea6489d1776904165
Nishankar <66866762+Nishan-Charlie@users.noreply.github.com>
2022-11-21 21:00:10 +0530

    aws connect

--- Backend/controllers/deviceFunction.js
+++ Backend/controllers/deviceFunction.js
@@ -6,10 +6,7 @@ require("events").EventEmitter.defaultMaxListners = 70;
 let mqtt = require('mqtt');
 
 process.env.TZ = "Asia/Calcutta";
-const clientId = "digitalHut_plug"
-                const options = {
-                    clientId,
-                }
+
 
 
 
@@ -26,7 +23,10 @@ let functions ={
             }
             console.log(req.body.state);
             let state = (req.body.state == 'true');
-            
+            const clientId = "digitalHut_plug"
+                const options = {
+                    clientId,
+                }
             let client = mqtt.connect("mqtt://127.0.0.1:1883", options);
             devices.findByIdAndUpdate(req.body.deviceid, {status: state}, (err, doc)=>{
                 if (err) return res.json({ success: false, msg: err.message })
@@ -102,7 +102,10 @@ let functions ={
                 if (err) return res.status(404).json({ success: false, msg: err.message });
                 if (!doc) return res.status(404).json({ success: false, msg: "Device Not found!" });
                 // If the device found
-                
+                const clientId = "digitalHut_RGB"
+                const options = {
+                    clientId,
+                }
                 let client = mqtt.connect("mqtt://127.0.0.1:1883", options);
             let dev ={state:state, brtns: req.body.brightness, port: parseInt(req.body.port), d_t: 1, r:r, g:g, b:b }
             console.log("Device Found")
@@ -150,12 +153,16 @@ let functions ={
             devices.findByIdAndUpdate(req.body.deviceid, { schedule: req.body.state, StartTime: StartTime, EndTime: EndTime }, (err, doc) => {
                 if (err) return res.status(404).json({ success: false, msg: err.message });
                 if (!doc) return res.status(404).json({ success: false, msg: "Device Not found!" });
-            
-                let client = mqtt.connect("mqtt://127.0.0.1:1883", options);
+                
                 // client.setMaxListeners(Infinity);
                 if (state) {
                     console.log(StartTime.getMinutes(), StartTime.getHours());
                     nodeSchedule.scheduleJob(req.body.deviceid + "start", `* ${StartTime.getMinutes()} ${StartTime.getHours()} * * *`, () => {
+                        const clientId = "digitalHut_Schedule"
+                        const options = {
+                            clientId,
+                        }
+                        let client = mqtt.connect("mqtt://127.0.0.1:1883", options);
                         devices.findByIdAndUpdate(req.body.deviceid, { status: true });
                         client.on('connect', function () {
                             console.log('Start Schedule');
@@ -178,6 +185,11 @@ let functions ={
                     // console.log(client.removeAllListeners('connect'));
                     console.log(EndTime.getMinutes(), EndTime.getHours());
                     nodeSchedule.scheduleJob(req.body.deviceid + "end", `* ${EndTime.getMinutes()} ${EndTime.getHours()} * * *`, () => {
+                        const clientId = "digitalHut_Schedule"
+                        const options = {
+                            clientId,
+                        }
+                        let client = mqtt.connect("mqtt://127.0.0.1:1883", options);
                         client.removeAllListeners('connect');
                         devices.findByIdAndUpdate(req.body.deviceid, { status: false });
                         client.on('connect', function () {
