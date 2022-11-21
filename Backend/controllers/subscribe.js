a2e830906e07e7e91ab7a3447dcc9171e99de015
Nishankar <66866762+Nishan-Charlie@users.noreply.github.com>
2022-11-22 01:44:11 +0530

    Scheduler

--- Backend/controllers/subscribe.js
+++ Backend/controllers/subscribe.js
@@ -13,7 +13,7 @@ const subscribe = async function () {
     try {
         let client = mqtt.connect("mqtt://127.0.0.1:1883", options);
         client.on('connect', () => {
-            console.log('Connected to MQTT broker...')
+            // console.log('Connected to MQTT broker...')
             client.subscribe([topic])
             // console.log(`Subscribed to ${topic}`)
         })
